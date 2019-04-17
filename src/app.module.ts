be30c8d3bc1963a83da8d69f58e05e0c6e0fde80
Mateus Durães <mduraes1994@gmail.com>
2019-04-17 17:56:54 -0300

    Import other modules in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { TagsModule } from './tags/tags.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PostsModule, TagsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
