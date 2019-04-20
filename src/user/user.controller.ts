b6619256287328b99d35e79fe588d31004834eb1
Mateus Durães <mduraes1994@gmail.com>
2019-04-20 00:10:58 -0300

    Registering, getting and updating users withouth auth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Body, Param } from '@nestjs/common';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+  constructor(
+    private readonly userService: UserService,
+  ) {}
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Post()
+  async register(@Body() user: User): Promise<User> {
+    return this.userService.register(user);
+  }
+
+  @Patch(':id')
+  async update(@Param() { id }: {id: number}, @Body() user): Promise<User> {
+    return this.userService.update(id, user);
+  }
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async register(user: User): Promise<User> {
+    return this.userRepository.save(user);
+  }
+
+  async update(id: number, user: Partial<User>): Promise<any> {
+    return this.userRepository.update(id as any, {
+      ...user as any,
+    });
+  }
+
+}
